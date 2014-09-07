b76ba6bc300042327394a02efdbbd8dfeec9e98a
emmack <eckotzer@gmail.com>
2014-09-07 16:39:58 -0400

    validations are working

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,18 +2,10 @@ class Booking < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :restaurant
 	validates :date, presence: true
-	validates :time, presence: true, uniqueness: {
-		scope: :date, :restaurant_id}
+
+	validates_uniqueness_of :time, scope: [:date, :restaurant_id], message: 'That time is unavailable, please try another.'
 
 
-		def size_fits
-			total_size = @restaurant.size
-			size_so_far = booking
 
-		booking = Booking.new
-		booking.valid?
-		person.errors.messages 
-		# => {:time=>["Time is already taken, please choose a new time.", "is too short (minimum is 3 characters)"]}
 
-	
 end
